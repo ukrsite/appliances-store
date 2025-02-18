2a7c4bddc641c5e4cc739ef466b03b6c681b5c20
SK <ukrsite@ukr.net>
2025-02-18 13:49:18 +0200

    Added OpenTelemetry

--- src/main/java/com/epam/rd/autocode/assessment/appliances/ApplianceStoreSpringApplication.java
+++ src/main/java/com/epam/rd/autocode/assessment/appliances/ApplianceStoreSpringApplication.java
@@ -1,5 +1,7 @@
 package com.epam.rd.autocode.assessment.appliances;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
+import com.microsoft.applicationinsights.connectionstring.ConnectionString;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApplianceStoreSpringApplication {
 
     public static void main(String[] args) {
+        ApplicationInsights.attach();
+        ConnectionString.configure("InstrumentationKey=55197219-9543-4bd0-b707-e90043784e4e;IngestionEndpoint=https://canadacentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://canadacentral.livediagnostics.monitor.azure.com/;ApplicationId=89f652e5-c479-4f23-b46e-608d1675ff0b");
         SpringApplication.run(ApplianceStoreSpringApplication.class, args);
     }
 }
